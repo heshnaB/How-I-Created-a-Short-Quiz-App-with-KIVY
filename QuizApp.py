2e621f471508a5a8a9a18c1bc3ad88e2b5ecdaee
heshnaB <heshna599@gmail.com>
2023-02-09 09:30:56 -0700

    Almost done

--- QuizApp.py
+++ QuizApp.py
@@ -4,10 +4,8 @@
 Author: Heshna Bhagawan
 Project: Building a quiz app that helps you decide what to eat.
 Ref: 
-- https://levelup.gitconnected.com/create-your-app-with-flutter-in-5-days-412ee41de22a
--If you need help with installing Android IOS Emulator: https://www.geeksforgeeks.org/how-to-set-up-an-emulator-for-vscode/
-- https://kivy.org/doc/stable/guide/widgets.html
-- https://www.section.io/engineering-education/kivy-python-first-app/
+Doc for Kivy: https://kivy.org/doc/stable/guide/widgets.html
+App Sample in Kivy: https://www.section.io/engineering-education/kivy-python-first-app/
 - Color picker: https://htmlcolorcodes.com/
 '''
 
@@ -25,6 +23,7 @@ from datetime import datetime, date, time, timedelta
 
 # Now we will open the file with the list of places and convert it into a list.
 p = open("Places.txt"); places = []; [places.append(i.strip()) for i in p]
+q = open("Questions.txt"); qs = [];  [qs.append(i.strip()) for i in q]
 
 # Split the list into the categories drive thru and deliver.
 drive_thru = places[0:6]; deliver = places[6:-1]
@@ -40,23 +39,14 @@ class QuizApp(App):
         self.window.pos_hint = {"center_x": 0.5, "center_y": 0.5}
         self.window.add_widget(Image(source="logo.png"))
         
-        self.ageRequest = Label(
+        self.ready4Quiz = Label(
             text = "Are you ready to decide what to eat?",
             font_size = 45,
             color = "#FFFFFF",
             bold = True
         )
         
-        self.window.add_widget(self.ageRequest)
-        
-        # self.date = TextInput(
-        #     multiline = False,
-        #     padding_y = (30,30),
-        #     size_hint = (1,0.7),
-        #     font_size = 30
-        # )
-        
-        # self.window.add_widget(self.date)
+        self.window.add_widget(self.ready4Quiz)
         
         self.button = Button(
             text = "YES, I'M STARVING!!",
@@ -65,22 +55,34 @@ class QuizApp(App):
             font_size = 30
         )
         
-        self.button.bind(on_press = self.q_1)
+        self.button.bind(on_press = self.beginQ)
         self.window.add_widget(self.button)
         
         return self.window
     
-    def q_1(self, event):
-        today = datetime.today().year
-        dob = self.date.text
-        age = int(today) - int(dob)
-        self.ageRequest.text = "You are " + str(int(age)) + " years old"
+    def beginQ(self, event):
+        text = "Do you feel like driving?"
+        self.window.remove_widget(self.ready4Quiz)
+        self.window.remove_widget(self.button)
         
-    # def getAge(self, event):
-    #     today = datetime.today().year
-    #     dob = self.date.text
-    #     age = int(today) - int(dob)
-    #     self.ageRequest.text = "You are " + str(int(age)) + " years old"
+        self.quest = Label(
+            text = "Do you feel like driving?",
+            font_size = 45,
+            color = "#FFFFFF",
+            bold = True
+        )
+        
+        self.window.add_widget(self.quest)
+        
+        self.buttonY = Button(
+            text = "Yes",
+            size_hint = (0.5, 0.5),
+            bold = True,
+            font_size = 30
+        )
+        
+        self.buttonY.bind(on_press = self.beginQ)
+        self.window.add_widget(self.buttonY)
 
 if __name__ == "__main__":
     QuizApp().run()
